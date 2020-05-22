ba233d82ebedfe8177cb8aa8dd194396b074a727
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-05-22 11:15:01 -0700

    Blog-Post Service: create 2 endpoints, one to create a post and emit and event to event bus, another to handle events emited from event bus

--- 1-blog/posts/index.js
+++ 1-blog/posts/index.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
+// app.get('/posts', (req, res) => {
+//   res.send(posts);
+// });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
-  const { title } = req.body;
-  posts[id] = { id, title };
-  res.status(201).send(posts[id]);
+  const newPost = { id, title: req.body.title };
+  posts[id] = newPost;
+
+  await axios.post('http://127.0.0.1:4005/events', {
+    type: 'PostCreated',
+    data: newPost,
+  });
+
+  res.status(201).send(newPost);
 });
 
-app.listen(4000, () => console.log('Posts Server listening on port 4000'));
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+  res.send({});
+});
+
+app.listen(4000, () => console.log('POSTS Server listening on port 4000'));
